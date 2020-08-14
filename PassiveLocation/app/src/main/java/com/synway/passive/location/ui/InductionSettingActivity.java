299f927bc968a4444c7a3262771e63ad7fbaae57
libin <1993911441@qq.com>
2020-08-14 12:34:41 +0800

    2

---
+++ PassiveLocation/app/src/main/java/com/synway/passive/location/ui/InductionSettingActivity.java
@@ -0,0 +1,37 @@
+package com.synway.passive.location.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.synway.passive.location.R;
+import com.synway.passive.location.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class InductionSettingActivity extends BaseActivity {
+    @BindView(R.id.ivBack)
+    ImageView ivBack;
+
+    private Unbinder unbinder;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_induction);
+        unbinder = ButterKnife.bind(this);
+        initVies();
+    }
+
+    private void initVies(){
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
